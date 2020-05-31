d67346719c7621ac45d1e2211235634318ef88f2
avarlamova <59831804+avarlamova@users.noreply.github.com>
2020-05-31 22:39:49 +0300

    constants added, empty grid for game created

---
+++ constants.js
@@ -0,0 +1,3 @@
+const columns = 10;
+const rows = 20; 
+const block_size = 10;
--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 //creating a grid for game
 const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d')
+const ctx = canvas.getContext('2d');
+ctx.canvas.width = columns * block_size;
+ctx.canvas.height = rows * block_size;
+
+ctx.scale(block_size,block_size);
 
 for (i=0;i<121;i++) {
 let x = 10;
@@ -8,5 +12,12 @@ let y = 10;
 ctx.fillRect(x, x+10, y, y+10);
 x+=15;
 y+=15;
-console.log(x);
 }
+
+function getBoard() {
+    return Array.from(
+    {length: rows}, () => Array(columns).fill(0)
+    )
+}
+
+console.log(getBoard())
