e5beb77268dfb528b39e0494e7fde6b7566205ac
avarlamova <59831804+avarlamova@users.noreply.github.com>
2020-06-01 23:00:49 +0300

    tetrominoes initialized

--- main.js
+++ main.js
@@ -6,18 +6,62 @@ ctx.canvas.height = rows * block_size;
 
 ctx.scale(block_size,block_size);
 
-for (i=0;i<121;i++) {
-let x = 10;
-let y = 10; 
-ctx.fillRect(x, x+10, y, y+10);
-x+=15;
-y+=15;
-}
-
+//create gameboard
 function getBoard() {
     return Array.from(
     {length: rows}, () => Array(columns).fill(0)
     )
 }
 
-console.log(getBoard())
+//console.table(getBoard())
+
+//creating piece
+class Piece {
+    x;
+    y;
+    color;
+    shape;
+    ctx;
+    
+    constructor(ctx) {
+     this.ctx = ctx;
+      this.spawn();
+    }
+    
+    spawn() {
+      this.color = 'green';
+      this.shape = [
+        [1, 0, 0], 
+        [1, 2, 2], 
+        [0, 0, 0]
+      ];
+      
+      // Starting position.
+      this.x = 5;
+      this.y = 5;
+    }
+
+
+  draw() {
+    this.ctx.fillStyle = this.color;
+    this.shape.forEach((row, y) => {
+      row.forEach((value, x) => {
+        // this.x, this.y gives the left upper position of the shape
+        // x, y gives the position of the block in the shape
+        // this.x + x is then the position of the block on the board
+        if (value > 0) {
+          this.ctx.fillRect(this.x + x, this.y + y, 1, 1);
+        }
+      });
+    });
+  }
+  }
+
+
+  function play() {
+    let piece = new Piece(ctx);
+    piece.draw();
+    board.piece = piece;
+  }
+
+  play()
